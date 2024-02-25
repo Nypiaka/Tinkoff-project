cb8bb45d88418b5a70c97f5ea7a95b84d458f95b
Nypiaka <sswwaattvvaatt@gmail.com>
2024-02-25 21:23:17 +0300

    HW2: Removing copypaste

--- scrapper/src/main/java/edu/java/clients/AbstractClient.java
+++ scrapper/src/main/java/edu/java/clients/AbstractClient.java
@@ -1,19 +1,35 @@
 package edu.java.clients;
 
+import edu.java.dao.LinksToUpdateDao;
 import org.springframework.web.reactive.function.client.WebClient;
 
 public abstract class AbstractClient<T> {
     private final WebClient webClient;
 
-    protected abstract void onReceipt(String s, T dto);
+    private final LinksToUpdateDao dao;
+
+    protected abstract void log(String line);
+
+    private void onReceipt(String s, T dto) {
+        var lastModified = dao.get(s);
+        if (lastModified == null || !lastModified.equals(dtoToString(dto))) {
+            dao.save(s, dtoToString(dto));
+            log("Updates by link: " + s + ": " + dtoToString(dto));
+        } else {
+            log("No updates by link: " + s + ": " + dtoToString(dto));
+        }
+    }
 
     protected Class<T> classMono;
 
-    public AbstractClient(String baseUrl) {
+    public AbstractClient(String baseUrl, LinksToUpdateDao dao) {
+        this.dao = dao;
         this.webClient = WebClient.builder().baseUrl(baseUrl).build();
     }
 
     protected abstract String transform(String link);
+
+    protected abstract String dtoToString(T dto);
 
     public void fetch(String uri) {
         var link = transform(uri);
