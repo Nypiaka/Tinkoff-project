452de03b77d4c27d7f2c5c4db26de80617f0d472
Nypiaka <sswwaattvvaatt@gmail.com>
2024-03-09 18:45:36 +0300

    HW3: Fixes

--- scrapper/src/main/java/edu/java/clients/BotClient.java
+++ scrapper/src/main/java/edu/java/clients/BotClient.java
@@ -2,17 +2,23 @@ package edu.java.clients;
 
 import edu.java.utils.dto.ApiErrorResponse;
 import edu.java.utils.dto.LinkUpdate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Component
 public class BotClient {
 
     private final WebClient webClient;
 
+    @Value("${server.link}")
+    private String baseUrl;
+
     public BotClient(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("http://localhost:8080").build();
+        this.webClient = webClientBuilder.baseUrl(baseUrl).build();
     }
 
     public Mono<ApiErrorResponse> update(LinkUpdate request) {
