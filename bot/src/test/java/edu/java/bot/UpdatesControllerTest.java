38e1d4b3168a2d3784b84f0b8a6e4b9c1b88bcdc
Nypiaka <sswwaattvvaatt@gmail.com>
2024-03-24 10:15:17 +0300

    HW5-Base: Fixes

--- bot/src/test/java/edu/java/bot/UpdatesControllerTest.java
+++ bot/src/test/java/edu/java/bot/UpdatesControllerTest.java
@@ -1,10 +1,12 @@
 package edu.java.bot;
 
+import edu.java.bot.bot.LinksRefreshCheckerBot;
 import edu.java.bot.controllers.UpdatesController;
 import edu.java.bot.service.ChatsService;
 import edu.java.utils.dto.LinkUpdate;
 import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -35,17 +37,19 @@ public class UpdatesControllerTest {
         Mono<ResponseEntity<?>> result =
             controller.update(linkUpdate);
 
-        assertEquals(HttpStatus.OK, result.block().getStatusCode());
+        assertEquals(HttpStatus.OK, Objects.requireNonNull(result.block()).getStatusCode());
         assertTrue(visited.get());
     }
 
     @Test
     void testUpdate_Failure() {
-        var chatsService = Mockito.mock(ChatsService.class);
+        var bot = Mockito.mock(LinksRefreshCheckerBot.class);
+        var chatsService = new ChatsService(bot);
         UpdatesController controller = new UpdatesController(chatsService);
-        doThrow(RuntimeException.class).when(chatsService).updateChatsInfo(any());
+        doThrow(RuntimeException.class).when(bot).execute(any());
         Mono<ResponseEntity<?>> result =
-            controller.update(new LinkUpdate(123L, URI.create("http://example.com"), "description", List.of(1L)));
-        assertEquals(HttpStatus.BAD_REQUEST, result.block().getStatusCode());
+            controller.update(new LinkUpdate(
+                123L, URI.create("http://example.com"), "description", List.of(1L)));
+        assertEquals(HttpStatus.BAD_REQUEST, Objects.requireNonNull(result.block()).getStatusCode());
     }
 }
