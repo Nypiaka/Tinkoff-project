a17942bd042069c71046ba4803ab1dc2a98b93cf
Nypiaka <sswwaattvvaatt@gmail.com>
2024-03-24 05:41:48 +0300

    HW5-Base: Fixes

--- scrapper/src/test/java/edu/java/controllers/ChatsControllerTest.java
+++ scrapper/src/test/java/edu/java/controllers/ChatsControllerTest.java
@@ -2,8 +2,12 @@ package edu.java.controllers;
 
 import edu.java.service.JdbcLinksService;
 import edu.java.utils.dto.ApiErrorResponse;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,24 +19,15 @@ public class ChatsControllerTest {
 
     private static final long TEST_ID = 123L;
 
-    @Test
-    void testRegisterChat_Success() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1})
+    void testRegisterChat(int ind) {
+        var params = List.of(Map.entry(true, HttpStatus.OK), Map.entry(false, HttpStatus.BAD_REQUEST));
         JdbcLinksService jdbcLinksService = Mockito.mock(JdbcLinksService.class);
-        when(jdbcLinksService.registerChat(TEST_ID)).thenReturn(true);
-
-        ChatsController controller = new ChatsController(jdbcLinksService);
-
-        Mono<ResponseEntity<ApiErrorResponse>> result = controller.registerChat(TEST_ID);
-        assertEquals(HttpStatus.OK, Objects.requireNonNull(result.block()).getStatusCode());
-    }
-
-    @Test
-    void testRegisterChat_Failure() {
-        JdbcLinksService jdbcLinksService = Mockito.mock(JdbcLinksService.class);
-        when(jdbcLinksService.registerChat(TEST_ID)).thenReturn(false);
+        when(jdbcLinksService.registerChat(TEST_ID)).thenReturn(params.get(ind).getKey());
         ChatsController controller = new ChatsController(jdbcLinksService);
         Mono<ResponseEntity<ApiErrorResponse>> result = controller.registerChat(TEST_ID);
-        assertEquals(HttpStatus.BAD_REQUEST, Objects.requireNonNull(result.block()).getStatusCode());
+        assertEquals(params.get(ind).getValue(), Objects.requireNonNull(result.block()).getStatusCode());
     }
 
     @Test
@@ -41,26 +36,21 @@ public class ChatsControllerTest {
         when(jdbcLinksService.removeChat(TEST_ID)).thenReturn(true);
         ChatsController controller = new ChatsController(jdbcLinksService);
         Mono<ResponseEntity<ApiErrorResponse>> result = controller.removeChat(TEST_ID);
-        assertEquals(HttpStatus.OK, result.block().getStatusCode());
+        assertEquals(HttpStatus.OK, Objects.requireNonNull(result.block()).getStatusCode());
     }
 
-    @Test
-    void testRemoveChat_NotFound() {
+    @ParameterizedTest
+    @ValueSource(
+        ints = {0, 1}
+    )
+    void testRemoveChat_Porblems(int ind) {
+        var params = List.of(Map.entry(true, HttpStatus.BAD_REQUEST), Map.entry(false, HttpStatus.NOT_FOUND));
         JdbcLinksService jdbcLinksService = Mockito.mock(JdbcLinksService.class);
         when(jdbcLinksService.removeChat(TEST_ID)).thenReturn(false);
-        when(jdbcLinksService.containsChat(TEST_ID)).thenReturn(false);
+        when(jdbcLinksService.containsChat(TEST_ID)).thenReturn(params.get(ind).getKey());
         ChatsController controller = new ChatsController(jdbcLinksService);
         Mono<ResponseEntity<ApiErrorResponse>> result = controller.removeChat(TEST_ID);
-        assertEquals(HttpStatus.NOT_FOUND, result.block().getStatusCode());
+        assertEquals(params.get(ind).getValue(), result.block().getStatusCode());
     }
 
-    @Test
-    void testRemoveChat_BadRequest() {
-        JdbcLinksService jdbcLinksService = Mockito.mock(JdbcLinksService.class);
-        when(jdbcLinksService.removeChat(TEST_ID)).thenReturn(false);
-        when(jdbcLinksService.containsChat(TEST_ID)).thenReturn(true);
-        ChatsController controller = new ChatsController(jdbcLinksService);
-        Mono<ResponseEntity<ApiErrorResponse>> result = controller.removeChat(TEST_ID);
-        assertEquals(HttpStatus.BAD_REQUEST, result.block().getStatusCode());
-    }
 }
