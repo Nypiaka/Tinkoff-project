7fc91e529e7cf6368c69f5240a2ebcb3728ce141
Nypiaka <sswwaattvvaatt@gmail.com>
2024-04-07 14:42:56 +0300

    HW8: retries addition

--- bot/src/main/java/edu/java/bot/configuration/KafkaConfiguration.java
+++ bot/src/main/java/edu/java/bot/configuration/KafkaConfiguration.java
@@ -4,13 +4,18 @@ import edu.java.utils.dto.LinkUpdate;
 import jakarta.validation.constraints.NotNull;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @ConfigurationProperties(prefix = "app")
 @ConfigurationPropertiesScan
@@ -28,6 +33,18 @@ public record KafkaConfiguration(
             ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class
         ), new LongDeserializer(), new JsonDeserializer<>(LinkUpdate.class).ignoreTypeHeaders()));
         return factory;
+    }
+
+    @Bean
+    public KafkaTemplate<Long, LinkUpdate> defaultRetryTopicKafkaTemplate() {
+        return new KafkaTemplate<>(
+            new DefaultKafkaProducerFactory<>(Map.of(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.addresses,
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class
+            )
+            )
+        );
     }
 
     public record Kafka(String addresses, String linkUpdateTopic, String linkUpdateGroup, String concurrency) {
--- bot/src/main/java/edu/java/bot/listeners/LinkUpdatesKafkaListener.java
+++ bot/src/main/java/edu/java/bot/listeners/LinkUpdatesKafkaListener.java
@@ -4,7 +4,12 @@ import edu.java.bot.service.ChatsService;
 import edu.java.utils.dto.LinkUpdate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +18,9 @@ import org.springframework.stereotype.Service;
 @ConfigurationProperties
 public class LinkUpdatesKafkaListener {
     private final ChatsService chatsService;
+    private final KafkaTemplate<Long, LinkUpdate> defaultRetryTopicKafkaTemplate;
 
+    @RetryableTopic
     @KafkaListener(topics = "#{@'app-edu.java.bot.configuration.KafkaConfiguration'.kafka.linkUpdateTopic}",
                    groupId = "#{@'app-edu.java.bot.configuration.KafkaConfiguration'.kafka.linkUpdateGroup}",
                    containerFactory = "linkUpdateKafkaListenerContainerFactory",
@@ -24,4 +31,8 @@ public class LinkUpdatesKafkaListener {
         chatsService.updateChatsInfo(linkUpdate);
     }
 
+    @DltHandler
+    public void listenDltLinkUpdates(LinkUpdate linkUpdate, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        defaultRetryTopicKafkaTemplate.send(topic.substring(0, topic.lastIndexOf("-dlt")) + "_dlq", linkUpdate);
+    }
 }
